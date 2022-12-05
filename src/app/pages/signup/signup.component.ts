64153eeb7f189b8de377fdc8328d07de882ab038
EmonterrosoF <monterrosoedras.@gmail.com>
2022-12-05 10:58:09 -0600

    first commit dev

---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,53 @@
+import  Swal  from 'sweetalert2';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from './../../services/user.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  public user = {
+    username : '',
+    password : '',
+    nombre : '',
+    apellido : '',
+    email : '',
+    telefono : ''
+  }
+
+  constructor(private userService:UserService,private snack:MatSnackBar, private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  formSubmit(){
+    console.log(this.user);
+    if(this.user.username == '' || this.user.username == null){
+      this.snack.open('El nombre de usuario es requerido !!','Aceptar',{
+        duration : 3000,
+        verticalPosition : 'top',
+        horizontalPosition : 'right'
+      });
+      return;
+    }
+
+    this.userService.añadirUsuario(this.user).subscribe(
+      (data) => {
+        console.log(data);
+        Swal.fire('Usuario guardado','Usuario registrado con exito en el sistema','success');
+        this.router.navigate(['login']);
+      },(error) => {
+        console.log(error);
+        this.snack.open('Ha ocurrido un error en el sistema !!','Aceptar',{
+          duration : 3000
+        });
+      }
+    )
+  }
+
+}
---
+++ src/app/services/helper.ts
@@ -0,0 +1,6 @@
+import { environment } from "src/environments/environment";
+
+
+
+let baserUrl = environment.BASE_URL
+export default baserUrl;
